6e07712f2bdcee770776f29bec66113992fd1376
jacktulv <shkinglu@163.com>
2017-11-23 21:22:53 +0800

    finish originEdition

--- designs.js
+++ designs.js
@@ -3,8 +3,43 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+let height, width,color;
 
-// Your code goes here!
+
+
+  $('button').click(function(){
+    height=$('#input_height').val();
+    width=$('#input_width').val();
+    makeGrid();
+    //获取颜色
+    $("#colorPicker").change(function(){
+        color=$(this).val();
+    });
+
+
+    $("td").click(function(){
+      $(this).css("background-color",color);
+    });
+  });
+
+
+
+
+
+/*
+@para
+*/
+
+function makeGrid() {
+      color=$('#colorPicker').val();
+      $('#pixel_canvas').html("");
+
+      for(let i=0;i<height;i++)
+      $('#pixel_canvas').append('<tr></tr>');
+      for(let j=0;j<width;j++)
+      $('tr').append('<td></td>');
+
+
+
 
 }
